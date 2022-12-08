634594ddf9c1bf408d88dd13dbf6d05a732bb9ec
Fahmi Fachrizal <34985368+fahmifachrizal@users.noreply.github.com>
2022-12-08 14:37:04 +0700

    3

---
+++ migrations/20221208071133-create-order.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      UserId: {
+        type: Sequelize.INTEGER
+      },
+      MasseusId: {
+        type: Sequelize.INTEGER
+      },
+      orderDate: {
+        type: Sequelize.DATE
+      },
+      duration: {
+        type: Sequelize.INTEGER
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Orders');
+  }
+};
---
+++ models/order.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Order extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Order.belongsTo(models.User)
+    }
+  }
+  Order.init({
+    UserId: DataTypes.INTEGER,
+    MasseusId: DataTypes.INTEGER,
+    orderDate: DataTypes.DATE,
+    duration: DataTypes.INTEGER,
+    status: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Order',
+  });
+  return Order;
+};
